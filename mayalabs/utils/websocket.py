6bb777e71788ce41e64227d547239bb1ed18a433
shubham-root <shubham.root@gmail.com>
2023-03-28 12:39:44 +0530

    corrected socket awaits

--- mayalabs/utils/websocket.py
+++ mayalabs/utils/websocket.py
@@ -3,6 +3,7 @@ import websockets
 import json
 import sys
 from colorama import init, Fore, Back, Style
+from ..mayalabs import authenticate
 
 init()
 
@@ -20,8 +21,6 @@ deploy_events = [
     'notification/node/#',
     # 'event-log/#'
 ]
-
-temp_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImY1OTNkMzYwNCJ9.eyJhdWQiOiIzMzI3MmJlOS1kOWQwLTQxNTEtODJiMy0yYmM3MjRlMDllNjgiLCJleHAiOjE2ODIyNDg0NDEsImlhdCI6MTY3OTY1NjQ0MSwiaXNzIjoibWF5YWxhYnMuaW8iLCJzdWIiOiIzMjM4YmM1Yy1kMjZhLTRmZjQtYjZjNS1lMmNjNTE4ZDMxMmMiLCJqdGkiOiI4MTdkMzU0MS0xZmYyLTQ5MGItYjlhMS00ZWE4MTRmMTYwY2MiLCJhdXRoZW50aWNhdGlvblR5cGUiOiJHT09HTEUiLCJlbWFpbCI6ImR1c2h5YW50OTMwOUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXBwbGljYXRpb25JZCI6IjMzMjcyYmU5LWQ5ZDAtNDE1MS04MmIzLTJiYzcyNGUwOWU2OCIsInJvbGVzIjpbXSwiYXV0aF90aW1lIjoxNjc5NjU2NDQxLCJ0aWQiOiI2MWUxNDQyMS03NDJlLWQxZGUtNDMzOC1hMDM5OWEzYzA4MzQiLCJwcm9maWxlU2x1ZyI6IjMyMzhiYzVjLWQyNmEtNGZmNC1iNmM1LWUyY2M1MThkMzEyYyIsImFjY2VzcyI6W3siZXhwIjo5OTk5OTk5OTk5OTk5LCJwZXJtaXNzaW9ucyI6IkFETUlOIiwic2x1ZyI6IjMyMzhiYzVjLWQyNmEtNGZmNC1iNmM1LWUyY2M1MThkMzEyYyIsInRpZXIiOiJQUkVNSVVNIiwidHJpYWxEYXlzIjoxNX0seyJleHAiOjk5OTk5OTk5OTk5OTksInBlcm1pc3Npb25zIjoiQURNSU4iLCJzbHVnIjoibWF5YWhxIiwidGllciI6IlBSRU1JVU0iLCJ0cmlhbERheXMiOjE1fSx7ImV4cCI6OTk5OTk5OTk5OTk5OSwicGVybWlzc2lvbnMiOiJBRE1JTiIsInNsdWciOiJheWFtIiwidGllciI6IkZSRUUiLCJ0cmlhbERheXMiOjE0fSx7ImV4cCI6OTk5OTk5OTk5OTk5OSwicGVybWlzc2lvbnMiOiJBRE1JTiIsInNsdWciOiJheWFtMiIsInRpZXIiOiJGUkVFIiwidHJpYWxEYXlzIjoxNH0seyJleHAiOjk5OTk5OTk5OTk5OTksInBlcm1pc3Npb25zIjoiQURNSU4iLCJzbHVnIjoiYXlhbTMiLCJ0aWVyIjoiRlJFRSIsInRyaWFsRGF5cyI6MTR9LHsiZXhwIjo5OTk5OTk5OTk5OTk5LCJwZXJtaXNzaW9ucyI6IkFETUlOIiwic2x1ZyI6ImF5YW00IiwidGllciI6IkZSRUUiLCJ0cmlhbERheXMiOjE0fSx7ImV4cCI6OTk5OTk5OTk5OTk5OSwicGVybWlzc2lvbnMiOiJBRE1JTiIsInNsdWciOiJheWFtNSIsInRpZXIiOiJGUkVFIiwidHJpYWxEYXlzIjoxNH0seyJleHAiOjk5OTk5OTk5OTk5OTksInBlcm1pc3Npb25zIjoiQURNSU4iLCJzbHVnIjoiYXlhbTYiLCJ0aWVyIjoiRlJFRSIsInRyaWFsRGF5cyI6MTR9LHsiZXhwIjo5OTk5OTk5OTk5OTk5LCJwZXJtaXNzaW9ucyI6IkFETUlOIiwic2x1ZyI6ImJyYWgiLCJ0aWVyIjoiRlJFRSIsInRyaWFsRGF5cyI6MTR9LHsiZXhwIjo5OTk5OTk5OTk5OTk5LCJwZXJtaXNzaW9ucyI6IkFETUlOIiwic2x1ZyI6IjM3IiwidGllciI6IkZSRUUiLCJ0cmlhbERheXMiOjE0fV0sImhhc0FjY2VzcyI6InllcyJ9.lMln2YMEAwfcht4GodipN4iFydCcB7ts_0rIhgAz7bY'
 
 # maya_log_prefix = Fore.MAGENTA + '[Maya]' + Style.RESET_ALL
 maya_log_prefix = '[Maya]'
@@ -86,14 +85,14 @@ class WebsocketListener:
 
 
                 
-    
-    async def start_listener(self, events=execution_events, log_prefix=maya_log_prefix):
+    @authenticate
+    async def start_listener(self, events=execution_events, log_prefix=maya_log_prefix, api_key=None):
         try:
             async with websockets.connect(self.url) as websocket:
                 # print('Connected to worker at', self.url)
                 self.websocket = websocket
                 
-                await websocket.send(json.dumps({ 'auth': temp_key }))
+                await websocket.send(json.dumps({ 'auth': api_key }))
                 auth_response = await websocket.recv()
                 # print('Auth response:', auth_response)
 
