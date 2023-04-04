c504a353bfe92aa9a0e1f652e1af04c9e26169f0
sibeshkar <sibesh96@gmail.com>
2023-04-04 15:15:08 +0530

    defaults added

--- mayalabs/consts.py
+++
@@ -1,6 +0,0 @@
-api_base_url = "https://api.dev.mayalabs.io"
-
-backend_base_url = "https://api.dev.mayalabs.io/app"
-# backend_base_url = "http://localhost:5000"
-
-api_ws_url = "wss://paccomms.pac.dev.mayalabs.io/socket"
