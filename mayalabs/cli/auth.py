8e4373eef33600150e1e551f036f7fb38ce4e48c
Alabhya Jindal <52493077+alabhyajindal@users.noreply.github.com>
2023-05-03 12:50:05 +0000

    Auth working - login, logout and status

--- mayalabs/cli/auth.py
+++ mayalabs/cli/auth.py
@@ -13,55 +13,63 @@ def auth(subcommand):
     elif subcommand == 'logout':
         logout()
     elif subcommand == 'status':
-        status()
+        user_name = status()
+        if user_name:
+            print(f'Authenticated as {user_name}')
+        else:
+            print('Not logged in')
     else:
         show_usage_guide()
 
 def login():
-    print('login handler here')
-
-def logout():
-    print('logout handler here')
-
-def status():
-    print('status handler here')
-
-def show_usage_guide():
-    print('USAGE')
-
-def set_key(api_key):
-    "Sets the API key provided using the -k option."
+    api_key = input('Please enter your API key: ')
     file_json = {"MAYA_API_KEY": api_key}
-
-    # Check if file exists
     if os.path.isfile(MAYA_CACHE_FILE):
-        # Read existing data
         with open(MAYA_CACHE_FILE, "r", encoding='UTF-8') as f:
             data = json.load(f)
             f.close()
-        # Update data
         data["MAYA_API_KEY"] = api_key
-        # Write back to file
         with open(MAYA_CACHE_FILE, "w", encoding='UTF-8') as f:
             f.write(json.dumps(data))
             f.close()
     else:
-        # Create new file
         with open(MAYA_CACHE_FILE, "w", encoding='UTF-8') as f:
             f.write(json.dumps(file_json))
             f.close()
+    user_name = status()
+    if user_name:
+        print(f'Authenticated as {user_name}')
+    else:
+        print('Invalid API Key')
 
-def whoami():
-    "Display information about the user."
+def logout():
+    user_name = status()
+    if user_name:
+        print(f'Logging out {user_name}')
+    else:
+        print('Not logged in')
+        return
+    if os.path.isfile(MAYA_CACHE_FILE):
+        with open(MAYA_CACHE_FILE, "r", encoding='UTF-8') as f:
+            data = json.load(f)
+            f.close()
+        del data["MAYA_API_KEY"]
+        with open(MAYA_CACHE_FILE, "w", encoding='UTF-8') as f:
+            f.write(json.dumps(data))
+            f.close()   
+    print('Logged out')
+
+def status():
     url = f"{default_api_base_url()}/app/v2/profiles/whoami"
-    payload={}
     api_key = get_api_key(prompt_if_missing=False)
     if api_key:
         headers = { 'X-API-KEY': api_key }
-        response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, timeout=30)
         response_dict = json.loads(response.text)
         name_value = response_dict["name"]
-        print(name_value)
+        return name_value
     else:
-        print(Style.BRIGHT + Fore.RED + 'You have not provided an API key.' + Style.RESET_ALL)
-        print("You can set the API key using mayalabs set -k '<YOUR_API_KEY>'")
+        return None
+
+def show_usage_guide():
+    print('USAGE')
