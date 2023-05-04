dfa4d891a3fde10cb309edbb1d488e4c79ce23b0
shubham-root <shubham.root@gmail.com>
2023-05-04 18:15:01 +0530

    updated branching

--- examples/04_branching_and_remerging/README.md
+++ examples/04_branching_and_remerging/README.md
@@ -15,10 +15,10 @@ import mayalabs
 
 fn = mayalabs.Function(name="Branching_Simple")
 script = """
-1. set {{msg.payload}} to 20
-    - 1.1. add '5' to {{msg.payload}}
+1. set {{msg.payload}} to 30
+    - 1.1. add 31 to {{msg.payload}}
     1.2. print {{msg.payload}}
-    - 1.3. add '10' to {{msg.payload}}
+    - 1.3. add 45 to {{msg.payload}}
     1.4. print {{msg.payload}}
 """
 fn.update(script=script)
--- examples/04_branching_and_remerging/branching_1.py
+++ examples/04_branching_and_remerging/branching_1.py
@@ -1,12 +1,13 @@
 import mayalabs
-
+mayalabs.api_key = "mayakey-$2a$10$mBaPxpG3wUkqWOEJtz4ssOj1TmsWOUvivzgrCYSbB/pp/tVDPjy8C"
+mayalabs.api_base = "https://api.dev.mayalabs.io"
 fn = mayalabs.Function(name="Branching_Simple_1")
-
+fn.clear()
 script = """
-1. set {{msg.payload}} to 20
-    - 1.1. add '5' to {{msg.payload}}
+1. set {{msg.payload}} to 30
+    - 1.1. add 31 to {{msg.payload}}
     1.2. print {{msg.payload}}
-    - 1.3. add '10' to {{msg.payload}}
+    - 1.3. add 45 to {{msg.payload}}
     1.4. print {{msg.payload}}
 """
 
