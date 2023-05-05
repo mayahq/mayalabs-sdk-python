08d180de1f58a09c115ae0367ea4cf4d7b97e11c
Alabhya Jindal <52493077+alabhyajindal@users.noreply.github.com>
2023-05-05 12:43:29 +0000

    Modifying teach for recipe Skill verification working with in memory zip

--- mayalabs/cli/teach.py
+++ mayalabs/cli/teach.py
@@ -1,6 +1,8 @@
 import os
 import json
 import requests
+import io
+import zipfile
 import mayalabs
 from halo import Halo
 from .helpers import get_api_key
@@ -18,15 +20,36 @@ def teach(file_path):
     mayalabs.api_key = api_key
 
     if os.path.isfile(file_path):
+        # create a BytesIO object to hold the zip file in memory
+        buffer = io.BytesIO()
+
+        # create a ZipFile object using the buffer as the file
+        with zipfile.ZipFile(
+            buffer, mode="w", compression=zipfile.ZIP_DEFLATED
+        ) as zip_file:
+            # create the steps, docs, and recipes folders
+            zip_file.writestr("steps/", "")
+            zip_file.writestr("docs/", "")
+            zip_file.writestr("recipes/", "")
+
+            # create the recipe_1 folder and add the text file to it
+            zip_file.writestr("recipes/recipe_1/", "")
+            zip_file.write(file_path, "recipes/recipe_1/s0.txt")
+
+        # get the contents of the zip file from the buffer
+        zip_data = buffer.getvalue()
+
+        # do something with the zip data, like write it to a file or send it over the network
+
         # Checking collision levels
         collision_spinner = Halo(spinner="dots")
         collision_spinner.start()
         headers = {"X-API-KEY": api_key}
-        files = {"files": open(file_path, "rb")}
+        files = {"files": ("filename.zip", io.BytesIO(zip_data))}
         url = "https://api.dev.mayalabs.io/pac/v1/library/skill/verify"
         response = requests.post(url, headers=headers, files=files, timeout=30)
         response_text = json.loads(response.text)
-        verification_data = response_text["reference"]["steps"][0]
+        verification_data = response_text["reference"]["recipes"][0]
         collision_spinner.stop()
         max_mean_collision_score = 0
         for key, value in verification_data.items():
@@ -40,9 +63,9 @@ def teach(file_path):
         intent_to_teach = "y"
         if max_mean_collision_percentage > 50:
             print(
-                f"The uploaded skill has a clash of {round(max_mean_collision_percentage, 2)}% with an existing skill."
+                f"The uploaded recipe has a clash of {round(max_mean_collision_percentage, 2)}% with an existing recipe."
             )
-            intent_to_teach = input("Do you want to teach the skill or abort? [y/N] ")
+            intent_to_teach = input("Do you want to teach the recipe or abort? [y/N] ")
 
         # Teaching
         if intent_to_teach == "y" or intent_to_teach == "Y":
@@ -54,7 +77,8 @@ def teach(file_path):
             response = requests.post(url, headers=headers, files=files, timeout=30)
             teaching_spinner.stop()
             response_text = json.loads(response.text)
-            print("Skill taught! You can now use it your programs.")
+            print(response_text)
+            # print("Recipe taught! You can now use it your programs.")
         else:
             return
     else:
