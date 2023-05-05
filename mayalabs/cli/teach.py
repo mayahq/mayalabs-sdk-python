b66dc01773eb922a8fabba2a2d7054c8ff3f10b4
Alabhya Jindal <52493077+alabhyajindal@users.noreply.github.com>
2023-05-05 08:08:23 +0000

    Checking collision percentage Asking the user if they want to proceed after showing the collision percentage

--- mayalabs/cli/teach.py
+++ mayalabs/cli/teach.py
@@ -19,15 +19,26 @@ def teach(file_path):
     if os.path.isfile(file_path):
         headers = {"X-API-KEY": api_key}
         files = {"files": open(file_path, "rb")}
+
         # Checking collision levels
         url = "https://api.dev.mayalabs.io/pac/v1/library/skill/verify"
         response = requests.post(url, headers=headers, files=files, timeout=30)
         response_text = json.loads(response.text)
-        steps = response_text["reference"]["steps"]
-        print(steps)
+        verification_data = response_text["reference"]["steps"][0]
+        max_mean_collision_score = 0
+        for key, value in verification_data.items():
+            for i in range(len(value)):
+                mean_collision_score = value[i]["mean_collision_score"]
+                if mean_collision_score > max_mean_collision_score:
+                    max_mean_collision_score = mean_collision_score
 
-        max_collision = 0.0
-        # find the max_collision from the nested data
+        # Checking user's intent to teach
+        max_mean_collision_percentage = max_mean_collision_score * 100
+        print(
+            f"The uploaded skill has a clash {max_mean_collision_percentage}% with an existing skill."
+        )
+        intent_to_teach = input("Do you want to teach the skill or abort [y/N] ")
+        # print(intent_to_teach)
 
         # Teaching
         # url = "https://api.dev.mayalabs.io/pac/v1/library/skill/teach"
